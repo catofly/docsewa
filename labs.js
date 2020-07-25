78d479d1708ec6368932f11d372937a793eb2af3
jellyGlass Pvt Ltd <jellyglass@jellyGlasss-MacBook-Air.local>
2020-07-25 19:02:16 +0530

    first commit

---
+++ labs.js
@@ -0,0 +1,80 @@
+ghmc([{
+		"name": "Osmania",
+		"addr": "Himayath Nagar",
+		"phone": "+91 62816 51144",
+		"website": "",
+		"attr1": "RT-PCR, Chest-CT, Swab, Blood Sample",
+		"attr2": "At Home sample collection available"
+	},
+	{
+		"name": "King Kothi",
+		"addr": "Himayath Nagar",
+		"phone": "+91 62816 51144",
+		"website": "https://vijayadiagnostic.com/",
+		"attr1": "RT-PCR, Chest-CT, Swab, Blood Sample",
+		"attr2": "At Home sample collection available"
+	}
+]);
+
+corp([
+
+	{
+		"name": "Vijaya",
+		"addr": "Himayath Nagar",
+		"phone": "+91 62816 51144",
+		"website": "https://vijayadiagnostic.com/",
+		"attr1": "RT-PCR, Chest-CT, Swab, Blood Sample",
+		"attr2": "At Home sample collection available"
+	},
+
+	{
+		"name": "Yashoda",
+		"addr": "Sec-Bad",
+		"phone": "+91 62816 51144",
+		"website": "https://vijayadiagnostic.com/",
+		"attr1": "RT-PCR, Chest-CT, Swab, Blood Sample",
+		"attr2": "Home Svc Not available"
+	}
+
+])
+
+athome([{
+		"name": "Vijaya",
+		"addr": "Himayath Nagar",
+		"phone": "+91 62816 51144",
+		"website": "https://vijayadiagnostic.com/",
+		"attr1": "RT-PCR, Chest-CT, Swab, Blood Sample",
+		"attr2": "At Home sample collection available"
+	},
+
+	{
+		"name": "Yashoda",
+		"addr": "Sec-Bad",
+		"phone": "+91 62816 51144",
+		"website": "https://vijayadiagnostic.com/",
+		"attr1": "RT-PCR, Chest-CT, Swab, Blood Sample",
+		"attr2": "Home Svc Not available"
+	}
+])
+
+dists([
+
+	{
+		"name": "Chelmeda Hosp",
+		"addr": "KarimNagar",
+		"phone": "+91 62816 51144",
+		"website": "https://vijayadiagnostic.com/",
+		"attr1": "RT-PCR, Chest-CT, Swab, Blood Sample",
+		"attr2": ""
+	},
+
+	{
+		"name": "Gandhi GH",
+		"addr": "Warangal",
+		"phone": "+91 62816 51144",
+		"website": "https://vijayadiagnostic.com/",
+		"attr1": "RT-PCR, Chest-CT, Swab, Blood Sample",
+		"attr2": "Home Svc Not available"
+	}
+
+]);
