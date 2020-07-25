96aba17511f322947442a2d8b5ce82efba3eed81
jellyGlass Pvt Ltd <jellyglass@jellyGlasss-MacBook-Air.local>
2020-07-25 06:26:39 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,315 @@
+ghmc([{
+		"name": "AIG",
+		"phone": "+91 62816 51144",
+		"email": "Gachibowli",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Continental",
+		"phone": "+91 62816 51144",
+		"email": "Gachibowli",
+		"attr1": "Please call our Reception desk for availability of beds at +91 40 2222 3344",
+		"attr2": ""
+	},
+	{
+		"name": "Medicover Hospital",
+		"phone": "+91 62816 51144",
+		"email": "Gachibowli",
+		"attr1": "Adhar Card is mandatory at the time of Admission",
+		"attr2": ""
+	},
+	{
+		"name": "Sunshine",
+		"phone": "+91 62816 51144",
+		"email": "Gachibowli",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Care",
+		"phone": "+91 62816 51144",
+		"email": "Hitech City",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Basavatarakam Indo-American Cancer Hospitals",
+		"phone": "+91 62816 51144",
+		"email": "Banjara Hills",
+		"attr1": "",
+		"attr2": "Insurance holders must submit necessary documents ahead of admission"
+	},
+	{
+		"name": "Care",
+		"phone": "+91 62816 51144",
+		"email": "Banjara Hills",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Century Hospital",
+		"phone": "+91 62816 51144",
+		"email": "Banjara Hills",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Omega",
+		"phone": "+91 62816 51144",
+		"email": "Banjara Hills",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Star Hospitals",
+		"phone": "+91 62816 51144",
+		"email": "Banjara Hills",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Virinchi",
+		"phone": "+91 62816 51144",
+		"email": "Banjara Hills",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Aware Gleneagles Global Hospital",
+		"phone": "+91 62816 51144",
+		"email": "LB Nagar",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Apollo",
+		"phone": "+91 62816 51144",
+		"email": "Jublee Hills",
+		"attr1": "",
+		"attr2": ""
+	}
+]);
+
+corp([{
+		"name": "AIG",
+		"phone": "+91 62816 51144",
+		"email": "Gachibowli",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Continental",
+		"phone": "+91 62816 51144",
+		"email": "Gachibowli",
+		"attr1": "Please call our Reception desk for availability of beds at +91 40 2222 3344",
+		"attr2": ""
+	},
+	{
+		"name": "Medicover Hospital",
+		"phone": "+91 62816 51144",
+		"email": "Gachibowli",
+		"attr1": "Adhar Card is mandatory at the time of Admission",
+		"attr2": ""
+	},
+	{
+		"name": "Sunshine",
+		"phone": "+91 62816 51144",
+		"email": "Gachibowli",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Care",
+		"phone": "+91 62816 51144",
+		"email": "Hitech City",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Basavatarakam Indo-American Cancer Hospitals",
+		"phone": "+91 62816 51144",
+		"email": "Banjara Hills",
+		"attr1": "",
+		"attr2": "Insurance holders must submit necessary documents ahead of admission"
+	},
+	{
+		"name": "Care",
+		"phone": "+91 62816 51144",
+		"email": "Banjara Hills",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Century Hospital",
+		"phone": "+91 62816 51144",
+		"email": "Banjara Hills",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Omega",
+		"phone": "+91 62816 51144",
+		"email": "Banjara Hills",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Star Hospitals",
+		"phone": "+91 62816 51144",
+		"email": "Banjara Hills",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Virinchi",
+		"phone": "+91 62816 51144",
+		"email": "Banjara Hills",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Aware Gleneagles Global Hospital",
+		"phone": "+91 62816 51144",
+		"email": "LB Nagar",
+		"attr1": "",
+		"attr2": ""
+	},
+	{
+		"name": "Apollo",
+		"phone": "+91 62816 51144",
+		"email": "Jublee Hills",
+		"attr1": "",
+		"attr2": ""
+	}
+]);
+
+
+// ghmc([{
+// 		"name": "Raju",
+// 		"phone": "999999999",
+// 		"email": "rksagi@gmail.com"
+// 	},
+// 	{
+// 		"name": "radha",
+// 		"phone": "88888888",
+// 		"email": "radha@gmail.com"
+// 	},
+// 	{
+// 		"name": "Ramu",
+// 		"phone": "777777777",
+// 		"email": "ramu@gmail.commmmmmmmmmmmmmxxxxxxx xxxxxxxxxxxxxxxxxxx xxxxxxxxxxxx"
+// 	},
+// 	{
+// 		"name": "Raju",
+// 		"phone": "999999999",
+// 		"email": "rksagi@gmail.com"
+// 	},
+// 	{
+// 		"name": "radha",
+// 		"phone": "88888888",
+// 		"email": "radha@gmail.com"
+// 	},
+// 	{
+// 		"name": "Ramu",
+// 		"phone": "777777777",
+// 		"email": "ramu@gmail.com"
+// 	},
+// 	{
+// 		"name": "Raju",
+// 		"phone": "999999999",
+// 		"email": "rksagi@gmail.com"
+// 	},
+// 	{
+// 		"name": "radha",
+// 		"phone": "88888888",
+// 		"email": "radha@gmail.com"
+// 	},
+// 	{
+// 		"name": "Ramu",
+// 		"phone": "777777777",
+// 		"email": "ramu@gmail.com"
+// 	},
+// 	{
+// 		"name": "Raju",
+// 		"phone": "999999999",
+// 		"email": "rksagi@gmail.com"
+// 	},
+// 	{
+// 		"name": "radha",
+// 		"phone": "88888888",
+// 		"email": "radha@gmail.com"
+// 	},
+// 	{
+// 		"name": "Ramu",
+// 		"phone": "777777777",
+// 		"email": "ramu@gmail.com"
+// 	},
+// 	{
+// 		"name": "Ramu",
+// 		"phone": "777777777",
+// 		"email": "ramu@gmail.com"
+// 	},
+// ]);
+
+// corp([{
+// 		"name": "Raju",
+// 		"phone": "999999999",
+// 		"email": "rksagi@gmail.com"
+// 	},
+// 	{
+// 		"name": "radha",
+// 		"phone": "88888888",
+// 		"email": "radha@gmail.com"
+// 	},
+// 	{
+// 		"name": "Ramu",
+// 		"phone": "777777777",
+// 		"email": "ramu@gmail.com"
+// 	},
+// 	{
+// 		"name": "Raju",
+// 		"phone": "999999999",
+// 		"email": "rksagi@gmail.com"
+// 	},
+// 	{
+// 		"name": "radha",
+// 		"phone": "88888888",
+// 		"email": "radha@gmail.com"
+// 	},
+// 	{
+// 		"name": "Ramu",
+// 		"phone": "777777777",
+// 		"email": "ramu@gmail.com"
+// 	},
+// 	{
+// 		"name": "Raju",
+// 		"phone": "999999999",
+// 		"email": "rksagi@gmail.com"
+// 	},
+// 	{
+// 		"name": "radha",
+// 		"phone": "88888888",
+// 		"email": "radha@gmail.com"
+// 	},
+// 	{
+// 		"name": "Ramu",
+// 		"phone": "777777777",
+// 		"email": "ramu@gmail.com"
+// 	},
+// 	{
+// 		"name": "Raju",
+// 		"phone": "999999999",
+// 		"email": "rksagi@gmail.com"
+// 	},
+// 	{
+// 		"name": "radha",
+// 		"phone": "88888888",
+// 		"email": "radha@gmail.com"
+// 	},
+// 	{
+// 		"name": "Ramu",
+// 		"phone": "777777777",
+// 		"email": "ramu@gmail.com"
+// 	},
+// ]);
