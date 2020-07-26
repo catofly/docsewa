7a91b0cc4c15024a88389d25725d61c12693eb04
jellyGlass Pvt Ltd <jellyglass@jellyGlasss-MacBook-Air.local>
2020-07-26 15:48:09 +0530

    first commit

---
+++ data/covidpackages.js
@@ -0,0 +1,54 @@
+Hyderabad([{
+		"provider": "Care Hospitals",
+		"pkgname": "Plan A - 7 Days",
+		"phone": "+91 62816 51144",
+		"website": "",
+		"attr1": "3 online consultations, Daily Nurse, Masks & other supplies",
+		"attr2": "Pulse Oximeter comes w this package"
+	},
+	{
+		"provider": "Care Hospitals",
+		"pkgname": "Plan B - 7 Days",
+		"phone": "+91 62816 51144",
+		"website": "",
+		"attr1": "RT-PCR, Chest-CT, Swab, Blood Sample",
+		"attr2": "At Home sample collection available"
+	},
+	{
+		"provider": "Vijaya Hospitals",
+		"pkgname": "Plan A - 7 Days",
+		"phone": "+91 62816 51144",
+		"website": "",
+		"attr1": "3 online consultations, Daily Nurse, Masks & other supplies",
+		"attr2": "Pulse Oximeter comes w this package"
+	},
+	{
+		"provider": "Vijaya Hospitals",
+		"pkgname": "Plan B - 7 Days",
+		"phone": "+91 62816 51144",
+		"website": "",
+		"attr1": "RT-PCR, Chest-CT, Swab, Blood Sample",
+		"attr2": "At Home sample collection available"
+	}
+
+]);
+
+OtherDistricts([
+
+	{
+		"provider": "Dr. Praveen,  Karimnagar",
+		"pkgname": "Plan A - 7 Days",
+		"phone": "+91 62816 51144",
+		"website": "",
+		"attr1": "3 online consultations, Daily Nurse, Masks & other supplies",
+		"attr2": "Pulse Oximeter comes w this package"
+	},
+	{
+		"provider": "Dr. Srinivas,  Jagtial",
+		"pkgname": "Plan A - 7 Days",
+		"phone": "+91 62816 51144",
+		"website": "",
+		"attr1": "3 online consultations, Daily Nurse, Masks & other supplies",
+		"attr2": "Pulse Oximeter comes w this package"
+	}
+]);
