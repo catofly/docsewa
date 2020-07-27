a68df1ff94fc5199e2d47d16bc7dcf6c1ed5fe09
jellyGlass Pvt Ltd <jellyglass@jellyGlasss-MacBook-Air.local>
2020-07-27 17:35:45 +0530

    phone

--- data/hospitals.js
+++ data/hospitals.js
@@ -1,5 +1,6 @@
 ghmc([{
 		"name": "AIG",
+		"url": "https://aighospitals.com/",
 		"phone": "+91 62816 51144",
 		"email": "Gachibowli",
 		"attr1": "",
@@ -7,6 +8,7 @@ ghmc([{
 	},
 	{
 		"name": "Continental",
+		"url": "https://aighospitals.com/",
 		"phone": "+91 62816 51144",
 		"email": "Gachibowli",
 		"attr1": "Please call our Reception desk for availability of beds at +91 40 2222 3344",
@@ -14,6 +16,7 @@ ghmc([{
 	},
 	{
 		"name": "Medicover Hospital",
+		"url": "https://aighospitals.com/",
 		"phone": "+91 62816 51144",
 		"email": "Gachibowli",
 		"attr1": "Adhar Card is mandatory at the time of Admission",
@@ -21,71 +24,9 @@ ghmc([{
 	},
 	{
 		"name": "Sunshine",
+		"url": "https://aighospitals.com/",
 		"phone": "+91 62816 51144",
 		"email": "Gachibowli",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Care",
-		"phone": "+91 62816 51144",
-		"email": "Hitech City",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Basavatarakam Indo-American Cancer Hospitals",
-		"phone": "+91 62816 51144",
-		"email": "Banjara Hills",
-		"attr1": "",
-		"attr2": "Insurance holders must submit necessary documents ahead of admission"
-	},
-	{
-		"name": "Care",
-		"phone": "+91 62816 51144",
-		"email": "Banjara Hills",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Century Hospital",
-		"phone": "+91 62816 51144",
-		"email": "Banjara Hills",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Omega",
-		"phone": "+91 62816 51144",
-		"email": "Banjara Hills",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Star Hospitals",
-		"phone": "+91 62816 51144",
-		"email": "Banjara Hills",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Virinchi",
-		"phone": "+91 62816 51144",
-		"email": "Banjara Hills",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Aware Gleneagles Global Hospital",
-		"phone": "+91 62816 51144",
-		"email": "LB Nagar",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Apollo",
-		"phone": "+91 62816 51144",
-		"email": "Jublee Hills",
 		"attr1": "",
 		"attr2": ""
 	}
@@ -93,6 +34,7 @@ ghmc([{
 
 corp([{
 		"name": "AIG",
+		"url": "https://aighospitals.com/",
 		"phone": "+91 62816 51144",
 		"email": "Gachibowli",
 		"attr1": "",
@@ -100,6 +42,7 @@ corp([{
 	},
 	{
 		"name": "Continental",
+		"url": "https://aighospitals.com/",
 		"phone": "+91 62816 51144",
 		"email": "Gachibowli",
 		"attr1": "Please call our Reception desk for availability of beds at +91 40 2222 3344",
@@ -107,6 +50,7 @@ corp([{
 	},
 	{
 		"name": "Medicover Hospital",
+		"url": "https://aighospitals.com/",
 		"phone": "+91 62816 51144",
 		"email": "Gachibowli",
 		"attr1": "Adhar Card is mandatory at the time of Admission",
@@ -118,198 +62,39 @@ corp([{
 		"email": "Gachibowli",
 		"attr1": "",
 		"attr2": ""
-	},
-	{
-		"name": "Care",
-		"phone": "+91 62816 51144",
-		"email": "Hitech City",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Basavatarakam Indo-American Cancer Hospitals",
-		"phone": "+91 62816 51144",
-		"email": "Banjara Hills",
-		"attr1": "",
-		"attr2": "Insurance holders must submit necessary documents ahead of admission"
-	},
-	{
-		"name": "Care",
-		"phone": "+91 62816 51144",
-		"email": "Banjara Hills",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Century Hospital",
-		"phone": "+91 62816 51144",
-		"email": "Banjara Hills",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Omega",
-		"phone": "+91 62816 51144",
-		"email": "Banjara Hills",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Star Hospitals",
-		"phone": "+91 62816 51144",
-		"email": "Banjara Hills",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Virinchi",
-		"phone": "+91 62816 51144",
-		"email": "Banjara Hills",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Aware Gleneagles Global Hospital",
-		"phone": "+91 62816 51144",
-		"email": "LB Nagar",
-		"attr1": "",
-		"attr2": ""
-	},
-	{
-		"name": "Apollo",
-		"phone": "+91 62816 51144",
-		"email": "Jublee Hills",
-		"attr1": "",
-		"attr2": ""
 	}
 ]);
 
-
-// ghmc([{
-// 		"name": "Raju",
-// 		"phone": "999999999",
-// 		"email": "rksagi@gmail.com"
-// 	},
-// 	{
-// 		"name": "radha",
-// 		"phone": "88888888",
-// 		"email": "radha@gmail.com"
-// 	},
-// 	{
-// 		"name": "Ramu",
-// 		"phone": "777777777",
-// 		"email": "ramu@gmail.commmmmmmmmmmmmmxxxxxxx xxxxxxxxxxxxxxxxxxx xxxxxxxxxxxx"
-// 	},
-// 	{
-// 		"name": "Raju",
-// 		"phone": "999999999",
-// 		"email": "rksagi@gmail.com"
-// 	},
-// 	{
-// 		"name": "radha",
-// 		"phone": "88888888",
-// 		"email": "radha@gmail.com"
-// 	},
-// 	{
-// 		"name": "Ramu",
-// 		"phone": "777777777",
-// 		"email": "ramu@gmail.com"
-// 	},
-// 	{
-// 		"name": "Raju",
-// 		"phone": "999999999",
-// 		"email": "rksagi@gmail.com"
-// 	},
-// 	{
-// 		"name": "radha",
-// 		"phone": "88888888",
-// 		"email": "radha@gmail.com"
-// 	},
-// 	{
-// 		"name": "Ramu",
-// 		"phone": "777777777",
-// 		"email": "ramu@gmail.com"
-// 	},
-// 	{
-// 		"name": "Raju",
-// 		"phone": "999999999",
-// 		"email": "rksagi@gmail.com"
-// 	},
-// 	{
-// 		"name": "radha",
-// 		"phone": "88888888",
-// 		"email": "radha@gmail.com"
-// 	},
-// 	{
-// 		"name": "Ramu",
-// 		"phone": "777777777",
-// 		"email": "ramu@gmail.com"
-// 	},
-// 	{
-// 		"name": "Ramu",
-// 		"phone": "777777777",
-// 		"email": "ramu@gmail.com"
-// 	},
-// ]);
-
-// corp([{
-// 		"name": "Raju",
-// 		"phone": "999999999",
-// 		"email": "rksagi@gmail.com"
-// 	},
-// 	{
-// 		"name": "radha",
-// 		"phone": "88888888",
-// 		"email": "radha@gmail.com"
-// 	},
-// 	{
-// 		"name": "Ramu",
-// 		"phone": "777777777",
-// 		"email": "ramu@gmail.com"
-// 	},
-// 	{
-// 		"name": "Raju",
-// 		"phone": "999999999",
-// 		"email": "rksagi@gmail.com"
-// 	},
-// 	{
-// 		"name": "radha",
-// 		"phone": "88888888",
-// 		"email": "radha@gmail.com"
-// 	},
-// 	{
-// 		"name": "Ramu",
-// 		"phone": "777777777",
-// 		"email": "ramu@gmail.com"
-// 	},
-// 	{
-// 		"name": "Raju",
-// 		"phone": "999999999",
-// 		"email": "rksagi@gmail.com"
-// 	},
-// 	{
-// 		"name": "radha",
-// 		"phone": "88888888",
-// 		"email": "radha@gmail.com"
-// 	},
-// 	{
-// 		"name": "Ramu",
-// 		"phone": "777777777",
-// 		"email": "ramu@gmail.com"
-// 	},
-// 	{
-// 		"name": "Raju",
-// 		"phone": "999999999",
-// 		"email": "rksagi@gmail.com"
-// 	},
-// 	{
-// 		"name": "radha",
-// 		"phone": "88888888",
-// 		"email": "radha@gmail.com"
-// 	},
-// 	{
-// 		"name": "Ramu",
-// 		"phone": "777777777",
-// 		"email": "ramu@gmail.com"
-// 	},
-// ]);
+otherdistricts([{
+		"name": "AIG",
+		"url": "https://aighospitals.com/",
+		"phone": "+91 62816 51144",
+		"email": "Gachibowli",
+		"attr1": "Karimnagar",
+		"attr2": ""
+	},
+	{
+		"name": "Continental",
+		"url": "https://aighospitals.com/",
+		"phone": "+91 62816 51144",
+		"email": "Gachibowli",
+		"attr1": "Warangal",
+		"attr2": ""
+	},
+	{
+		"name": "Medicover Hospital",
+		"url": "https://aighospitals.com/",
+		"phone": "+91 62816 51144",
+		"email": "Gachibowli",
+		"attr1": "Adhar Card is mandatory at the time of Admission",
+		"attr2": ""
+	},
+	{
+		"name": "Sunshine",
+		"url": "https://aighospitals.com/",
+		"phone": "+91 62816 51144",
+		"email": "Gachibowli",
+		"attr1": "Metpalli",
+		"attr2": ""
+	}
+]);
